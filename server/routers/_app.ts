51ac0e8ee9e4dd1eb65e7946d9e5d4ad1f11be3a
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-04-05 10:18:47 +0800

    add a middleware to wrap Prisma rejected errors and replace with proper TRPCError

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,12 +1,13 @@
-import { createRouter as createCRUDRouter } from './generated/routers';
-import { initTRPC } from '@trpc/server';
-import { type Context } from '../context';
-import superjson from 'superjson';
 import {
     PrismaClientKnownRequestError,
     PrismaClientUnknownRequestError,
     PrismaClientValidationError,
 } from '@prisma/client/runtime';
+import { TRPCError, initTRPC } from '@trpc/server';
+import { isPrismaClientKnownRequestError } from '@zenstackhq/runtime';
+import superjson from 'superjson';
+import { type Context } from '../context';
+import { createRouter as createCRUDRouter } from './generated/routers';
 
 function makePrismaError(error: Error | undefined) {
     if (error instanceof PrismaClientKnownRequestError) {
@@ -41,6 +42,29 @@ const t = initTRPC.context<Context>().create({
     },
 });
 
-export const appRouter = createCRUDRouter(t.router, t.procedure);
+// middleware for wrapping access denied errors from enhanced Prisma
+// to proper TRPC errors with 403 code
+const errorWrappedProc = t.procedure.use(async ({ next }) => {
+    console.log('Calling middleware');
+    const result = await next();
+    if (
+        !result.ok &&
+        isPrismaClientKnownRequestError(result.error.cause) &&
+        result.error.cause.code === 'P2004' // access denied
+    ) {
+        return {
+            ...result,
+            error: new TRPCError({
+                code: 'FORBIDDEN',
+                message: result.error.cause.message,
+                cause: result.error.cause,
+            }),
+        };
+    } else {
+        return result;
+    }
+});
+
+export const appRouter = createCRUDRouter(t.router, errorWrappedProc);
 
 export type AppRouter = typeof appRouter;
