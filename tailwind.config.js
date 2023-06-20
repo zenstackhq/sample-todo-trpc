a278acc479b7ebd7dae82ab21ebe373a86afcaa7
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-06-20 14:35:20 +0800

    update to latest zenstack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
         extend: {},
     },
     plugins: [require('daisyui'), require('@tailwindcss/line-clamp')],
+    daisyui: {
+        themes: false,
+    },
 };
